fd8562c5538ec2e6bb6652c4d2a8e1da3a1b7ebd
SoloistRoy <soloistroy@gmail.com>
2017-02-25 21:16:20 -0500

    Add files via upload

--- Collector/annualData.py
+++ Collector/annualData.py
@@ -1,22 +1,18 @@
 from yahoo_finance import Share
 from pymongo import MongoClient
 
-def getStockData(cName, collection):
+def get_annualData(cName, collection):
 	stock = Share(cName)
-	dataSet = stock.get_historical('2016-12-1','2017-2-1')
+	dataSet = stock.get_historical('2016-2-1','2017-2-1')
 	for item in dataSet:
-		post = {'datet':item['Date'], 'open':float(item['Open']),
+		post = {'date':item['Date'], 'open':float(item['Open']),
 				'close':float(item['Close']), 'high':float(item['High']),
 				'low':float(item['Low']), 'volume':int(item['Volume'])}
-		# posts = db.posts
 		post_id = collection.insert_one(post).inserted_id
-		print post_id
-	return dataSet
 
 dbClient = MongoClient()
 db = dbClient.StockAnnual
 
 stockList = ['YHOO', 'GOOG']
-for i in stockList:
-	collection = db[i]
-	getStockData(i, collection)
+for stock in stockList:
+	get_annualData(stock, db[stock])
--- Collector/realtimeData.py
+++ Collector/realtimeData.py
@@ -2,25 +2,25 @@ import datetime
 import time as T
 
 from yahoo_finance import Share
+from pymongo import MongoClient
 
-# find the date of yesterday
+def get_realtimeData(cName, collection):
+    stock = Share(cName)
+    post = {'datetime':stock.get_trade_datetime(), 'price':stock.get_price(),
+            'Volume':stock.get_volume()}
+    post_id = collection.insert_one(post).inserted_id
+
+
+dbClient = MongoClient()
+db = dbClient.StockRealtime
+
 now = datetime.datetime.now()
 
-# get the records ranging from max date in DB to yesterday
-BeginTime = now.replace(hour=9, minute=0, second=0, microsecond=0)
-EndTime = now.replace(hour=16, minute=0, second=0, microsecond=0)
-# LocalTime = T.strftime('%Y-%m-%d ',T.localtime(T.time()))
-# while now > BeginTime and now < EndTime:
+# beginTime = now.replace(hour=9, minute=0, second=0, microsecond=0)
+# endTime = now.replace(hour=16, minute=0, second=0, microsecond=0)
+stockList = ['YHOO', 'GOOG']
+# while now > beginTime and now < endTime:
 while 1:
-    now = datetime.datetime.now()
-    StockList = ['YHOO', 'GOOG']
-    for stock in StockList:
-        Symbol = stock
-        Company = Share(stock)
-        Company.refresh()
-        Price = Company.get_price()
-        Time = Company.get_trade_datetime()
-        Volume = Company.get_volume()
-        purchases = (Symbol, Price, Time, Volume)
-        print purchases
-    T.sleep(5)
+    for stock in stockList:
+        get_realtimeData(stock, db[stock])
+    T.sleep(60)
