dea87e1522517513d64453e9c12975c1e09c5d18
SoloistRoy <soloistroy@gmail.com>
2017-02-26 21:45:21 -0500

    Add files via upload

--- Collector/annualData.py
+++ Collector/annualData.py
@@ -2,17 +2,18 @@ from yahoo_finance import Share
 from pymongo import MongoClient
 
 def get_annualData(cName, collection):
+	collection.remove()
 	stock = Share(cName)
 	dataSet = stock.get_historical('2016-2-1','2017-2-1')
 	for item in dataSet:
 		post = {'date':item['Date'], 'open':float(item['Open']),
 				'close':float(item['Close']), 'high':float(item['High']),
 				'low':float(item['Low']), 'volume':int(item['Volume'])}
-		post_id = collection.insert_one(post).inserted_id
+		collection.insert_one(post)
 
 dbClient = MongoClient()
 db = dbClient.StockAnnual
 
-stockList = ['YHOO', 'GOOG']
+stockList = ['YHOO', 'GOOG', 'AAPL', 'BIDU', 'BABA']
 for stock in stockList:
 	get_annualData(stock, db[stock])
--- Collector/realtimeData.py
+++ Collector/realtimeData.py
@@ -3,12 +3,17 @@ import time as T
 
 from yahoo_finance import Share
 from pymongo import MongoClient
+from pymongo import errors
 
 def get_realtimeData(cName, collection):
     stock = Share(cName)
+    collection.create_index('datetime', unique = True)
     post = {'datetime':stock.get_trade_datetime(), 'price':stock.get_price(),
             'Volume':stock.get_volume()}
-    post_id = collection.insert_one(post).inserted_id
+    try:
+    	collection.insert_one(post)
+    except errors.DuplicateKeyError:
+    	print datetime.datetime.now()
 
 
 dbClient = MongoClient()
@@ -18,9 +23,12 @@ now = datetime.datetime.now()
 
 # beginTime = now.replace(hour=9, minute=0, second=0, microsecond=0)
 # endTime = now.replace(hour=16, minute=0, second=0, microsecond=0)
-stockList = ['YHOO', 'GOOG']
+stockList = ['YHOO', 'GOOG', 'AAPL', 'BIDU', 'BABA']
 # while now > beginTime and now < endTime:
+for i in stockList:
+	db[i].remove()
 while 1:
     for stock in stockList:
         get_realtimeData(stock, db[stock])
-    T.sleep(60)
+    print ++1
+    T.sleep(59.30)
