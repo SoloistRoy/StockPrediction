4fb72469582e3c652c91c02ead8da39b37b41bf5
SoloistRoy <soloistroy@gmail.com>
2017-03-23 15:39:08 -0400

    modified:   server/index.py

--- server/index.py
+++ server/index.py
@@ -1,22 +1,33 @@
-from flask import Flask
-from flask.ext.pymongo import PyMongo
-from pymongo import MongoClient
-from flask import render_template
+from flask import Flask, render_template, url_for, request, session, redirect
+from flask_pymongo import PyMongo
 
 app = Flask('StockAnnual', template_folder = 'G:\Python\Web\StockPrediction',static_folder='G:\Python\Web\StockPrediction')
-# app = Flask('StockAnnual', template_folder = '')
+app.config['MONGO_DBNAME'] = 'StockAnnual'
+app.config['MONGO_URI'] = 'mongodb://localhost/StockAnnual'
+app.config['SECRET_KEY'] = 'super secret key'
 
 mongo = PyMongo(app)
 # dbClient = MongoClient()
 # db = dbClient.StockRealtime
 
 @app.route('/')
-def hello_world():
-	# online_users = db.GOOG.find({'date': '2017-02-01'})
-	online_users = mongo.db.GOOG.find({'date': '2017-02-01'})
-	return render_template('index.html', online_users=online_users)
-	# return type(online_users)
-    # return 'Hello World!'
+def index():
+	if 'piece' in session:
+		m = session['piece']
+		session.clear()
+		return str(m)
+
+	# online_users = mongo.db.GOOG.find({'date': '2017-02-01'})
+	return render_template('index.html')
+
+@app.route('/query', methods=['POST'])
+def query():
+	stockName = request.form['stockName']
+	stock = mongo.db[stockName]
+	m = stock.find({})
+	session['piece'] = m[0][u'high']
+	return redirect(url_for('index'))
+	# return str(m[4][u'high'])
 
 if __name__ == '__main__':
 	app.debug = True
