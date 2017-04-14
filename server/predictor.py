ddd1cfceba977504640ba8397ec0ab90820908a7
SoloistRoy <soloistroy@gmail.com>
2017-04-13 21:29:15 -0400

    new file:   Predictor/cModelAAPL 	new file:   Predictor/hlModelAAPL 	new file:   Predictor/oModelAAPL 	new file:   Predictor/vModelAAPL 	new file:   server/predictor.py

---
+++ server/predictor.py
@@ -0,0 +1,11 @@
+from sklearn.svm import SVR
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.externals import joblib
+
+class annualPredict():
+	def  _init(self):
+		pass
+
+	def load(self):
+		pass
